b6c8d54dc78dcb94245912789a9cfb1107cbe05e
Smith, Jacob Tyler <jts11@iu.edu>
2024-03-26 19:30:39 -0400

    practicum 11

---
+++ src/main/java/edu/iu/jts11/primesservice/controller/AuthenticationController.java
@@ -0,0 +1,46 @@
+package edu.iu.jts11.primesservice.controller;
+
+import edu.iu.jts11.primesservice.model.Customer;
+import edu.iu.jts11.primesservice.service.IAuthenticationService;
+import edu.iu.jts11.primesservice.service.TokenService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class AuthenticationController {
+    private final IAuthenticationService authenticationService;
+    private final AuthenticationManager authenticationManager;
+    private TokenService tokenService;
+
+    public AuthenticationController(IAuthenticationService authenticationService, AuthenticationManager authenticationManager, TokenService tokenService) {
+        this.authenticationService = authenticationService;
+        this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/register")
+    public boolean register(@RequestBody Customer customer) {
+        try {
+            return authenticationService.register(customer);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody Customer customer) {
+        Authentication authentication = authenticationManager
+                .authenticate(
+                        new UsernamePasswordAuthenticationToken(
+                                customer.getUsername(),
+                                customer.getPassword())
+        );
+        return tokenService.generateToken(authentication);
+    }
+}
---
+++ src/main/java/edu/iu/jts11/primesservice/security/Jwks.java
@@ -0,0 +1,24 @@
+package edu.iu.jts11.primesservice.security;
+
+import com.nimbusds.jose.jwk.RSAKey;
+
+import java.security.KeyPair;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
+public class Jwks {
+    private Jwks() {
+    }
+
+    public static RSAKey generateRsa() {
+        RSAKey rsaKey;
+        KeyPair keyPair = KeyGeneratorUtils.generateRsaKey();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        return new RSAKey.Builder(publicKey)
+                .privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+}
