95d11d008bfb5f393c48c22c5de02d5a8f9b7102
Smith, Jacob Tyler <jts11@iu.edu>
2024-04-09 19:53:43 -0400

    practicum 13

--- src/main/java/edu/iu/jts11/primesservice/controller/PrimesController.java
+++ src/main/java/edu/iu/jts11/primesservice/controller/PrimesController.java
@@ -1,6 +1,9 @@
 package edu.iu.jts11.primesservice.controller;
 
+import edu.iu.jts11.primesservice.rabbitmq.MQSender;
 import edu.iu.jts11.primesservice.service.IPrimesService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,12 +11,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/primes")
 public class PrimesController {
     IPrimesService primesService;
-    public PrimesController(IPrimesService primesService) {
+    MQSender MQSender;
+
+    public PrimesController(IPrimesService primesService, MQSender mqSender) {
         this.primesService = primesService;
+        this.MQSender = mqSender;
     }
 
-    @GetMapping("{n}")
+    @GetMapping("/{n}")
     public boolean isPrime(@PathVariable int n) {
-        return primesService.isPrime(n);
+        System.out.println("isPrime: " + n);
+        boolean result = primesService.isPrime(n);
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username = ((Jwt) principal).getSubject();
+        System.out.println("username: " + username);
+        MQSender.sendMessage(username, n, result);
+        return result;
     }
 }
